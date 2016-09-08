911a14deb73d222fb8048b94280fee94f17c9180
objectreality <andy@objectreality.co.uk>
2016-09-08 01:25:31 +0100

    Styling

---
+++ client/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    return new Promise<Hero[]>(
+      resolve =>
+        setTimeout(resolve, 500)
+    ).then(() => HEROES);
+    }
+
+    getHero(id: number) : Promise<Hero> {
+      return this.getHeroes()
+                 .then(heroes => heroes.find(hero => hero.id == id));
+    }
+}
