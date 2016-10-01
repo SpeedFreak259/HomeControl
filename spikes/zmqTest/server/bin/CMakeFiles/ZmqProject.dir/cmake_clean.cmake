620d5f78b0e8c4669a21eadcfc57f41c6b98f3e6
Andy Baker <andy@objectreality.co.uk>
2016-10-01 19:12:27 +0000

    Added spikes folder

---
+++ spikes/zmqTest/server/bin/CMakeFiles/ZmqProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZmqProject.dir/main.cpp.o"
+  "ZmqProject.pdb"
+  "ZmqProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZmqProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
